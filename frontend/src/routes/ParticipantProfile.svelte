13a4553bd175f766975a56052e0faab331bdf7d2
Iván Raskovsky <raskovsky+git@gmail.com>
2025-08-15 14:53:10 -0300

    feat: add down arrow icon to Outdated badges
    - Added chevron-down icon to Outdated badge in Validators page
    - Updated ParticipantProfile to use down arrow instead of warning icon
    - Consistent visual language: ✓ for Current, ↓ for Outdated
    - Better visual indication of version status

--- frontend/src/routes/ParticipantProfile.svelte
+++ frontend/src/routes/ParticipantProfile.svelte
@@ -260,7 +260,7 @@
                     {:else}
                       <span class="inline-flex items-center px-3 py-1 rounded-full text-sm font-medium bg-amber-100 text-amber-800 border border-amber-200">
                         <svg class="w-4 h-4 mr-1.5" fill="currentColor" viewBox="0 0 20 20">
-                          <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+                          <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                         </svg>
                         Outdated
                       </span>
--- frontend/src/routes/Validators.svelte
+++ frontend/src/routes/Validators.svelte
@@ -216,6 +216,9 @@
                         </span>
                       {:else if validator.targetVersion}
                         <span class="inline-flex items-center px-2 py-0.5 rounded-full text-xs font-medium bg-amber-100 text-amber-800">
+                          <svg class="w-3 h-3 mr-0.5" fill="currentColor" viewBox="0 0 20 20">
+                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                          </svg>
                           Outdated
                         </span>
                       {/if}
