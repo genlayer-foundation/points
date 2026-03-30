3801b8c0448d95417ae5128d120ffd80fcc38743
JoaquinBN <joaquinbressan@gmail.com>
2026-03-30 13:11:30 +0200

    Add merge migration for contributions app
    Resolves conflicting leaf nodes (0033_submissionnote_data_field from
    submissions branch and 0040_convert_featured_images_to_cloudinary from
    dev) that caused App Runner deploys to roll back.

---
+++ backend/contributions/migrations/0041_merge_submissions_and_featured.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contributions', '0033_submissionnote_data_field'),
+        ('contributions', '0040_convert_featured_images_to_cloudinary'),
+    ]
+
+    operations = []
