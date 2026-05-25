fce8629167c5e486acf3ea4e1460472ed281e88d
JoaquinBN <joaquinbressan@gmail.com>
2026-05-25 17:10:40 +0200

    Merge contribution migration heads

---
+++ backend/contributions/migrations/0062_merge_0061_community_discord_xp_0061_featuredcontent_hero_placements.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("contributions", "0061_community_discord_xp"),
+        ("contributions", "0061_featuredcontent_hero_placements"),
+    ]
+
+    operations = []
