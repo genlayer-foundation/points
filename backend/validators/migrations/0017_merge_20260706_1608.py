edbe023b3d32bb435bb28a709542e8a9a13d240e
JoaquinBN <joaquinbressan@gmail.com>
2026-07-06 18:10:29 +0200

    Merge validator migration branches

---
+++ backend/validators/migrations/0017_merge_20260706_1608.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('validators', '0016_backfill_graduated_validator_profiles'),
+        ('validators', '0016_validatoroperatorwallet'),
+    ]
+
+    operations = []
